1060c69be5e6ae58998e28cdcf6cc1304e20dc22
rrredface <theredfaceone@gmail.com>
2017-04-14 13:33:24 -0500

    Added jisho command and HttpError variant

--- src/commands.rs
+++ src/commands.rs
@@ -1,11 +1,14 @@
 use help;
 use strokes;
+use jisho;
+use hyper;
 use discord::Discord;
 use discord::model::Message;
 
 pub enum CommandResult {
 	Success,
-	Syntax
+	Syntax,
+	HttpError(hyper::error::Error)
 }
 
 pub struct Command {
@@ -19,13 +22,20 @@ pub struct Command {
 impl Command {
 	pub fn exec(&self, s: &Discord, m: &Message, args: Vec<&str>){
 		let author = format!("{}", m.author.id);
+		let ch = m.channel_id;
 		if check_perms(author.as_str(), self.perm.as_str()) {
 			match (self.run)(s, m, args) {
 				CommandResult::Success => {},
 				CommandResult::Syntax => {
-					let ch = m.channel_id;
-					let help_msg = format!{"```{}```", self.help_txt};
-					let _ = s.send_message(ch, help_msg.as_str(), "", false);
+					let help_msg = &format!{"```{}```", self.help_txt};
+					let _ = s.send_message(ch, help_msg, "", false);
+				}
+				CommandResult::HttpError(e) => {
+					let _ = s.send_message(ch,
+						&format!{"An HTTP error occured while running {} :(", self.label},
+						"", false
+					);
+					println!("HTTP error while running {}:\n  {:?}", self.label, e);
 				}
 			}
 		}
@@ -47,6 +57,13 @@ lazy_static! {
 			help_txt: "Usage:\n  strokes <chars>\nExample:\n  strokes 猫".into(),
 			perm: "felix.strokes".into(),
 			run: strokes::strokes_cmd
+		},
+		Command {
+			label: "!jisho".into(),
+			desc: "Searches jisho.org for a given query.".into(),
+			help_txt: "Usage:\n  jisho <query>\nExample:\n  jisho 猫".into(),
+			perm: "felix.jisho".into(),
+			run: jisho::jisho_cmd
 		}
 	];
 }
@@ -69,7 +86,7 @@ fn check_perms(id: &str, perm: &str) -> bool {
 			return true
 		}
 	} else if PERMS.user_has_perm(id, perm) {
-		return true;
+		return true
 	}
 	false
 }
