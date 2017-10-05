da51001916e2b0684f33a8afe4d7eeb001568f09
rrredface <theredfaceone@gmail.com>
2017-10-05 03:42:33 -0500

    Added random command and new result types

--- src/commands.rs
+++ src/commands.rs
@@ -2,41 +2,49 @@ use help;
 use strokes;
 use jisho;
 use hyper;
+use serde_json;
 use discord::Discord;
 use discord::model::Message;
 
 pub enum CommandResult {
 	Success,
 	Syntax,
-	HttpError(hyper::error::Error)
+	InvalidArg(String),
+	HttpError(hyper::error::Error),
+	JsonError(serde_json::error::Error),
 }
 
 pub struct Command {
 	pub label: String,
 	pub desc: String,
 	pub help_txt: String,
-	pub perm: String,
 	pub run: fn(s: &Discord, m: &Message, args: Vec<&str>) -> CommandResult
 }
 
 impl Command {
 	pub fn exec(&self, s: &Discord, m: &Message, args: Vec<&str>){
-		let author = format!("{}", m.author.id);
+		//let author = format!("{}", m.author.id);
 		let ch = m.channel_id;
-		if check_perms(author.as_str(), self.perm.as_str()) {
-			match (self.run)(s, m, args) {
-				CommandResult::Success => {}
-				CommandResult::Syntax => {
-					let help_msg = &format!{"```{}```", self.help_txt};
-					let _ = s.send_message(ch, help_msg, "", false);
-				}
-				CommandResult::HttpError(e) => {
-					let _ = s.send_message(ch,
-						&format!{"An HTTP error occured while running {} :(", self.label},
-						"", false
-					);
-					println!("HTTP error while running {}:\n  {:?}", self.label, e);
-				}
+		match (self.run)(s, m, args) {
+			CommandResult::Success => {}
+			CommandResult::Syntax => {
+				let help_msg = &self.help_txt;
+				let _ = s.send_message(ch, &help_msg, "", false);
+			}
+			CommandResult::InvalidArg(msg) => {
+				let _ = s.send_message(ch, &msg, "", false);
+			}
+			CommandResult::HttpError(e) => {
+				let _ = s.send_message(ch,
+					&format!{"An error occured while running `{}` :(", self.label}, "", false
+				);
+				println!("HTTP error while running {}:\n  {:?}", self.label, e);
+			}
+			CommandResult::JsonError(e) => {
+				let _ = s.send_message(ch,
+					&format!{"An error occured while running `{}` :(", self.label}, "", false
+				);
+				println!("JSON error while running {}:\n  {:?}", self.label, e);
 			}
 		}
 	}
@@ -47,23 +55,26 @@ lazy_static! {
 		Command {
 			label: "!help".into(),
 			desc: "Shows this help message.".into(),
-			help_txt: "Usage:\n  help [command]\nExample:\n  help strokes".into(),
-			perm: "felix.help".into(),
+			help_txt: "Usage:\n`help [command]`\nExample:\n`help strokes`".into(),
 			run: help::help_cmd
 		},
 		Command {
 			label: "!strokes".into(),
 			desc: "Show the stroke order of a given character.".into(),
-			help_txt: "Usage:\n  strokes <chars>\nExample:\n  strokes 猫".into(),
-			perm: "felix.strokes".into(),
+			help_txt: "Usage:\n`strokes <chars>`\nExample:\n`strokes 猫`".into(),
 			run: strokes::strokes_cmd
 		},
 		Command {
 			label: "!jisho".into(),
 			desc: "Searches jisho.org for a given query.".into(),
-			help_txt: "Usage:\n  jisho <query>\nExample:\n  jisho 猫".into(),
-			perm: "felix.jisho".into(),
+			help_txt: "Usage:\n`jisho <query>`\nExample:\n`jisho 猫`".into(),
 			run: jisho::jisho_cmd
+		},
+		Command {
+			label: "!random".into(),
+			desc: "Grabs a random word from Jisho.".into(),
+			help_txt: "Usage:\n`random <jlpt-level>`\nExample:\n`random 5`".into(),
+			run: jisho::random_cmd
 		}
 	];
 }
@@ -77,16 +88,4 @@ pub fn parse_cmd(s: &Discord, m: &Message) {
 			break;
 		}
 	}
-}
-
-fn check_perms(id: &str, perm: &str) -> bool {
-	use PERMS;
-	if let Some(g) = PERMS.get_group("default") {
-		if PERMS.group_has_perm(g, perm) {
-			return true
-		}
-	} else if PERMS.user_has_perm(id, perm) {
-		return true
-	}
-	false
 }
